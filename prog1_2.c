7607747000cd12a986bdbc57c6e3f5c056926a13
Taylor Ewertz <t_ewertz@yahoo.com>
2016-09-01 14:23:17 -0700

    add prog1_2

---
+++ prog1_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	printf("Assignment #1-2, Taylor Ewertz, t_ewertz@yahoo.com\n");
+
+	char name[256];
+
+	printf("Hello! What is your name?\n");
+	scanf("%s",&name);
+	printf("Hello %s. Nice to meet you!\n",name);
+	return 0;
+}
